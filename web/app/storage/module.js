ea4ce29160e9ab2d2cc3c4452a0b6f8a77799a50
woorea <luis@woorea.es>
2012-09-10 19:50:35 +0200

    added support for creating containers and directories on swift

--- web/app/storage/module.js
+++ web/app/storage/module.js
@@ -2,7 +2,7 @@ var storage = angular.module("storage",[]);
 storage.config(function($routeProvider) {
 	$routeProvider
 		.when("/:tenant/storage/:region/containers", {controller : "ContainerListCtrl", templateUrl : "app/storage/views/containers/list.html"})
-		.when("/:tenant/storage/:region/containers/:name", {controller : "ContainerShowCtrl", templateUrl : "app/storage/views/containers/show.html"})
+		.when("/:tenant/storage/:region/containers/:container", {controller : "ContainerShowCtrl", templateUrl : "app/storage/views/containers/show.html"})
 });
 storage.controller("ContainerListCtrl",function($scope, $routeParams, OpenStack) {
 	
@@ -10,22 +10,17 @@ storage.controller("ContainerListCtrl",function($scope, $routeParams, OpenStack)
 	
 	$scope.onCreate = function() {
 		
+		
 		OpenStack.ajax({
 			method : "PUT",
 			url : endpoint + "/" + $scope.name
 		}).success(function(data, status, headers, config) {
 			$scope.name = '';
 			$scope.onRefresh();
-			/*
-			$scope.servers = [];
-			angular.forEach(data.servers, function(server) {
-				server.checked = false;
-				$scope.servers.push(server);
-			});
-			*/
 		}).error(function(data, status, headers, config) {
 
 		});
+		
 	}
 	
 	$scope.onRefresh = function() {
@@ -35,37 +30,43 @@ storage.controller("ContainerListCtrl",function($scope, $routeParams, OpenStack)
 			url : endpoint + "?format=json"
 		}).success(function(data, status, headers, config) {
 			$scope.containers = data;
-			/*
-			$scope.servers = [];
-			angular.forEach(data.servers, function(server) {
-				server.checked = false;
-				$scope.servers.push(server);
-			});
-			*/
 		}).error(function(data, status, headers, config) {
 
 		});
+		
 	}
 	
 	$scope.onRefresh();
 
 });
-storage.controller("ContainerShowCtrl",function($scope) {
+storage.controller("ContainerShowCtrl",function($scope, $routeParams, $http, OpenStack) {
+	
+	var endpoint = OpenStack.endpoint("object-store",$routeParams.region, "publicURL");
+	
+	$scope.onCreateDirectory = function() {
+		
+		OpenStack.ajax({
+			method : "PUT",
+			url : endpoint + "/" + $routeParams.container + "/" + $scope.name,
+			data : "",
+			headers : {
+				"Content-Type" : "application/directory"
+			}
+		}).success(function(data, status, headers, config) {
+			$scope.onRefresh();
+		}).error(function(data, status, headers, config) {
+
+		});
+		
+	};
 	
 	$scope.onRefresh = function() {
 		
 		OpenStack.ajax({
 			method : "GET",
-			url : endpoint + "/" + $routeParams.name + "?format=json"
+			url : endpoint + "/" + $routeParams.container + "?format=json"
 		}).success(function(data, status, headers, config) {
 			$scope.objects = data;
-			/*
-			$scope.servers = [];
-			angular.forEach(data.servers, function(server) {
-				server.checked = false;
-				$scope.servers.push(server);
-			});
-			*/
 		}).error(function(data, status, headers, config) {
 
 		});
